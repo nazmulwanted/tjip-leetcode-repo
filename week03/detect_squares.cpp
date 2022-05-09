7812c6f45445d4adffd206f5b17eff509d37c62d
Md. Nazmul Hossain <nazmulwanted@gmail.com>
2022-05-09 19:35:00 +0600

    Problem 12: Detect squares. Done.(a bit optimized.)

--- week03/detect_squares.cpp
+++ week03/detect_squares.cpp
@@ -1,7 +1,12 @@
 // Time complexity: O(N), N = number of points on the plane.
 // Space complexity: O(N)
 
-map<pair<int, int>, int> points;
+unordered_map<long long, int> points;
+int BASE = 1000000007;
+
+long long getSingleValue(pair<int, int> p){
+    return (1LL * p.first * BASE) + p.second;
+}
 
 DectectSquares{
     points.clear();
@@ -10,7 +15,7 @@ DectectSquares{
 void add(vector<int> point){
     int x = point[0];
     int y = point[1];
-    points[{x, y}]++;
+    points[getSingleValue({x, y})]++;
 }
 
 int count(vector<int> qpoint){
@@ -18,12 +23,14 @@ int count(vector<int> qpoint){
     for(auto point: points){
         int x2 = qpoint[0];
         int y2 = qpoint[1];
-        int x4 = point.first.first;
-        int y4 = point.first.second;
+        int x4 = point.first / BASE;
+        int y4 = point.first % BASE;
+        long long value_x4y2 = getSingleValue({x4, y2});
+        long long value_x2y4 = getSingleValue({x2, y4});
 
         if(x2 == x4 || (abs(x2 - x4) != abs(y2 - y4))) continue;
-        if(points.count({x4, y2}) && points.count({x2, y4})){
-            total += points[{x4, y2}] * points[{x2, y4}] * point.second;
+        if(points.count(value_x4y2) && points.count(value_x2y4)){
+            total += points[value_x4y2] * points[value_x2y4] * point.second;
         }
     }
     return total;
