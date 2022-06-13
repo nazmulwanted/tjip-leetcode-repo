0d5a07fa4a6444888bb28b50f7b33c2ce8fc9bd3
Md. Nazmul Hossain <nazmulwanted@gmail.com>
2022-06-13 20:59:01 +0600

    Problem 12: First bad version. Done.

---
+++ week07/first_bad_version.cpp
@@ -0,0 +1,13 @@
+// Time complexity: O(log N) , N = Number of versions.
+// Space complexity: O(1)
+
+int firstBadVersion(int n){
+    int left = 0;
+    int right = n;
+    while(left < right){
+        int mid = left + (right - left) / 2;
+        if(isBadVersion(mid)) right = mid;
+        else left = mid + 1;
+    }
+    return left;
+}
